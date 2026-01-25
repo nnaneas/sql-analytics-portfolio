e7eb992ebcf5b214af7b63d2e56437699d730fa0
nnaneas <nanebeglaran@gmail.com>
2026-01-25 20:49:59 +0400

    some classwork

--- queries/create_sales_analysis.sql
+++ queries/create_sales_analysis.sql
@@ -354,3 +354,243 @@ SET discount = (
 	FROM sales_analysis
 )
 WHERE discount IS NULL;
+
+---------
+DROP TABLE IF EXISTS customers_raw_text;
+
+CREATE TABLE customers_raw_text (
+  customer_id   INTEGER,
+  first_name    TEXT,
+  last_name     TEXT,
+  raw_phone     TEXT,
+  category_raw  TEXT,
+  birth_date    DATE
+);
+
+INSERT INTO customers_raw_text (
+  customer_id,
+  first_name,
+  last_name,
+  raw_phone,
+  category_raw,
+  birth_date
+) VALUES
+  (1, 'joHN',     'doE',        '   077600945  ',   'Accessories (Promo)', DATE '1994-03-12'),
+  (2, 'MARY',     'sMiTh',      '077-600-045',      'Electronics (Old)',   DATE '1988-11-05'),
+  (3, 'aLEx',     'johnSON',    '(374)-77-600-945', 'Accessories',         DATE '2001-07-23'),
+  (4, 'anna',     'VAN DYKE',   '37477600945',      'Electronics (Promo)', DATE '1999-01-30'),
+  (5, NULL,       'brOwn',      '77600945',         'Accessories (Test)',  DATE '1994-03-12');
+
+SELECT 
+	raw_phone,
+	LENGTH(raw_phone) AS length
+FROM customers_raw_text;
+
+SELECT ('HELLO');
+
+SELECT
+	raw_phone,
+	LENGTH(raw_phone) AS length,
+CASE
+	WHEN LENGTH(raw_phone) = 8 THEN 'everything is correct'
+	ELSE 'some clean up is needed'
+END AS result
+FROM customers_raw_text;
+
+SELECT 
+	raw_phone,
+	LENGTH(raw_phone) AS initial_length,
+	TRIM(raw_phone) AS trimmed,
+	LENGTH(TRIM(raw_phone)) AS trimmed_length
+FROM customers_raw_text;
+
+-- trimmed from the left side
+SELECT LTRIM(raw_phone)
+FROM customers_raw_text;
+
+-- trimmed from the right side
+SELECT RTRIM(raw_phone)
+FROM customers_raw_text;
+
+SELECT 
+	last_name,
+	UPPER(last_name) AS upper_name
+FROM customers_raw_text;
+
+SELECT 
+	last_name,
+	LOWER(last_name) AS upper_name
+FROM customers_raw_text;
+
+SELECT 
+	last_name,
+	INITCAP(last_name) AS upper_name
+FROM customers_raw_text;
+
+SELECT 
+	first_name,
+	last_name,
+	INITCAP(CONCAT(first_name, ' ', last_name)) AS full_name
+FROM customers_raw_text;
+-- or
+SELECT 
+	first_name,
+	last_name,
+	INITCAP(first_name || ' ' || last_name) AS full_name
+FROM customers_raw_text;
+
+SELECT 
+	raw_phone,
+	REPLACE(raw_phone, '-', '') AS replaced
+FROM customers_raw_text;
+
+SELECT 
+	raw_phone,
+	TRIM(REPLACE(raw_phone, '-', '')) AS replaced
+FROM customers_raw_text;
+
+SELECT
+  raw_phone,
+  REPLACE(
+    REPLACE(
+      REPLACE(TRIM(raw_phone), '-', ''),
+    '(', ''),
+  ')', '') AS phone_partial_clean
+FROM customers_raw_text;
+
+SELECT
+  raw_phone,
+  REGEXP_REPLACE(raw_phone, '[^0-9]', '', 'g') AS digits_only,
+  LENGTH(REGEXP_REPLACE(raw_phone, '[^0-9]', '', 'g')) AS length
+FROM customers_raw_text;
+
+SELECT
+    category_raw,
+    REGEXP_REPLACE(category_raw, '\([^)]*\)', '', 'g') AS category_clean
+FROM customers_raw_text;
+
+SELECT 
+	category_raw,
+	SUBSTRING(category_raw FROM 1 FOR 11) AS final_result
+FROM customers_raw_text;
+
+SELECT
+	first_name,
+	last_name,
+	INITCAP(CONCAT(first_name, ' ', last_name)) AS full_name
+FROM customers_raw_text;
+
+SELECT
+	raw_phone,
+	POSITION('-' IN raw_phone) AS position
+FROM customers_raw_text;
+-- or
+SELECT
+	raw_phone,
+	STRPOS(raw_phone, '-') AS position
+FROM customers_raw_text;
+
+SELECT 
+	raw_phone,
+	STRPOS(raw_phone, '(') > 0 AS result
+FROM customers_raw_text;
+
+SELECT 
+	raw_phone,
+	SPLIT_PART(raw_phone, '-', 3) AS result
+FROM customers_raw_text;
+
+SELECT 
+	first_name,
+	NULLIF(first_name, '') AS result
+FROM customers_raw_text;
+
+DROP TABLE IF EXISTS transactions_text_demo;
+
+CREATE TABLE transactions_text_demo (
+  transaction_id INTEGER,
+  customer_id    INTEGER,
+  raw_phone      TEXT,
+  category_raw   TEXT,
+  quantity       INTEGER,
+  price          NUMERIC(10,2)
+);
+
+INSERT INTO transactions_text_demo
+SELECT
+  gs AS transaction_id,
+  (RANDOM() * 200)::INT + 1 AS customer_id,
+
+  CASE (gs % 6)
+    WHEN 0 THEN '   077600945  '
+    WHEN 1 THEN '077-600-045'
+    WHEN 2 THEN '(374)-77-600-945'
+    WHEN 3 THEN '37477600945'
+    WHEN 4 THEN '77600945'
+    ELSE '077600945'
+  END AS raw_phone,
+
+  CASE (gs % 5)
+    WHEN 0 THEN 'Accessories (Promo)'
+    WHEN 1 THEN 'Accessories (Test)'
+    WHEN 2 THEN 'Electronics (Old)'
+    WHEN 3 THEN 'Electronics (Promo)'
+    ELSE 'Accessories'
+  END AS category_raw,
+
+  (RANDOM() * 5)::INT + 1 AS quantity,
+  (RANDOM() * 500 + 10)::NUMERIC(10,2) AS price
+FROM generate_series(1, 1000) AS gs;
+
+SELECT
+  COUNT(*) AS total_rows,
+  COUNT(DISTINCT raw_phone) AS distinct_raw_phones,
+  COUNT(DISTINCT category_raw) AS distinct_categories
+FROM transactions_text_demo;
+
+-- task1
+-- raw_phone
+SELECT 
+	raw_phone
+FROM transactions_text_demo;
+
+-- LENGTH(raw_phone)
+SELECT 
+	raw_phone,
+	LENGTH(raw_phone) AS phone_length
+FROM transactions_text_demo;
+
+-- position of '-'
+SELECT
+	raw_phone,
+	STRPOS(raw_phone, '-')
+FROM transactions_text_demo;
+
+-- position of '('
+SELECT
+	raw_phone,
+	STRPOS(raw_phone, '(')
+FROM transactions_text_demo;
+
+-- count of rows per pattern
+SELECT 
+	raw_phone,
+	COUNT(raw_phone),
+	STRPOS(raw_phone, '(')
+FROM transactions_text_demo
+GROUP BY raw_phone;
+--
+SELECT 
+	raw_phone,
+	COUNT(raw_phone),
+	STRPOS(raw_phone, '-')
+FROM transactions_text_demo
+GROUP BY raw_phone;
+
+--task2
+SELECT 
+	DISTINCT(category_raw),
+	COUNT(category_raw)
+FROM transactions_text_demo
+GROUP BY category_raw
+ORDER BY COUNT(category_raw) DESC;
