71adea6be68f5df229338061e4d9a94f4aa3c1ea
nnaneas <nanebeglaran@gmail.com>
2026-01-31 11:54:17 +0400

    joins

--- queries/create_sales_analysis.sql
+++ queries/create_sales_analysis.sql
@@ -707,3 +707,95 @@ FROM sales_analysis
 GROUP BY 
 	customer_name,
 	days_since_trans;
+
+SELECT
+  c.customer_id,
+  CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+  o.number_of_orders,
+  o.recent_order
+FROM analytics.customers c
+INNER JOIN (
+  SELECT
+    customer_id,
+    MAX(order_date) AS recent_order,
+    COUNT(customer_id) AS number_of_orders
+	FROM analytics.orders
+	GROUP BY customer_id
+) o
+  ON c.customer_id = o.customer_id;
+
+SELECT
+  c.*,
+  o.order_id
+FROM analytics.customers c
+LEFT JOIN (
+  SELECT *
+  FROM analytics.orders
+  WHERE customer_id < 10
+) o
+  ON c.customer_id = o.customer_id
+  WHERE o.customer_id IS NULL;
+
+SELECT
+  o.order_id,
+  p.product_name,
+  oi.quantity
+FROM analytics.orders o
+JOIN analytics.order_items oi
+  ON o.order_id = oi.order_id
+JOIN analytics.products p
+  ON oi.product_id = p.product_id;
+
+SELECT
+  o.order_id,
+  SUM(oi.quantity * p.price) AS order_revenue
+FROM analytics.orders o
+JOIN analytics.order_items oi
+  ON o.order_id = oi.order_id
+JOIN analytics.products p
+  ON oi.product_id = p.product_id
+GROUP BY o.order_id;
+
+SELECT
+  c.customer_id,
+  ci.city_name,
+  r.region_name,
+  co.country_name
+FROM analytics.customers c
+JOIN analytics.cities ci
+  ON c.city_id = ci.city_id
+JOIN analytics.regions r
+  ON ci.region_id = r.region_id
+JOIN analytics.countries co
+  ON r.country_id = co.country_id;
+
+SELECT
+  r.region_name,
+  co.country_name,
+  COUNT(customer_id) AS customers
+FROM analytics.customers c
+JOIN analytics.cities ci
+  ON c.city_id = ci.city_id
+JOIN analytics.regions r
+  ON ci.region_id = r.region_id
+JOIN analytics.countries co
+  ON r.country_id = co.country_id
+GROUP BY r.region_name,
+         co.country_name;
+
+SELECT
+  c.customer_id,
+  ci.city_name,
+  ST_Within(cl.geom, cb.geom) AS inside_city
+FROM analytics.customers c
+JOIN analytics.customer_locations cl
+  ON c.customer_id = cl.customer_id
+JOIN analytics.cities ci
+  ON c.city_id = ci.city_id
+JOIN analytics.city_boundaries cb
+  ON ci.city_id = cb.city_id;
+
+SELECT COUNT(*) AS joined_rows
+FROM analytics.orders o
+JOIN analytics.order_items oi
+  ON o.order_id = oi.order_id;
